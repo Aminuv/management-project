4617ba227618473ab0735f59f60b208505204566
Aminuv <mohamed.elmarbou07@gmail.com>
2025-12-09 17:44:29 +0100

    feat: refactor button and controle components for improved structure and styling

--- src/app/shared/controle/controle.component.ts
+++ src/app/shared/controle/controle.component.ts
@@ -1,11 +1,15 @@
-import { Component, input,  } from '@angular/core';
+import { Component, input, ViewEncapsulation,  } from '@angular/core';
 
 @Component({
   selector: 'app-controle',
   standalone: true,
   imports: [],
   templateUrl: './controle.component.html',
-  styleUrl: './controle.component.css'
+  styleUrl: './controle.component.css',
+  encapsulation: ViewEncapsulation.None,
+  host: {
+    class: 'control'
+  },
 })
 export class ControleComponent {
   label = input.required<string>();
