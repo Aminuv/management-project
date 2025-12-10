d87a731aea516c921f16ed5202da4bbd25c660d4
Aminuv <mohamed.elmarbou07@gmail.com>
2025-12-10 15:49:47 +0100

    feat: enhance server status component with dynamic status updates and refactor ticket and traffic components for improved structure

--- src/app/dashboard/server-status/server-status.component.ts
+++ src/app/dashboard/server-status/server-status.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-server-status',
@@ -7,6 +7,30 @@ import { Component } from '@angular/core';
   templateUrl: './server-status.component.html',
   styleUrl: './server-status.component.css'
 })
-export class ServerStatusComponent {
-  currentStatus = 'online';
+export class ServerStatusComponent implements OnInit {
+  currentStatus: 'online' | 'offline' | 'unknown' = 'offline';
+  private interval?: ReturnType<typeof setInterval>;
+
+  constructor() {
+  
+  }
+
+  ngOnInit() {
+    this.interval = setInterval(() => {
+      const random = Math.random();
+      if (random < 0.5) {
+        this.currentStatus = 'online';
+      } else if (random < 0.9) {
+        this.currentStatus = 'offline';
+      } else {
+        this.currentStatus = 'unknown';
+      }
+    }, 5000);
+  }
+
+  ngAfterViewDestroy() {
+    //if (this.interval) {
+    //  clearInterval(this.interval);
+    //}
+  }
 }
